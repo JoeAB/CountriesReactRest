2333f3ddce41fb8d668fef7eed7ce298535f1280
Joe <joe.andrew.bennett@gmail.com>
2021-10-09 16:46:08 -0500

    got leaflet maps working and set up a basic map using the latitude and longitude coordinates from the country api to set the position values

--- CountriesReactRest/ClientApp/src/components/Country.js
+++ CountriesReactRest/ClientApp/src/components/Country.js
@@ -1,5 +1,6 @@
 ﻿import React, { Component, useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom'
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 
 
@@ -16,12 +17,30 @@ export function Country() {
     }, []);
 
     if (data) {
+        const position = [data[0].latlng[0], data[0].latlng[1]]
         return (
-            <div className="row">
-                <span className="col-md-6"><h1>{data[0].name.common}</h1></span>
-                <span className="col-md-6"><img className="Flag" src={'' + data[0].flags.png} /></span>
+            <>
                 
-            </div>
+                <div className="row">
+                    <span className="col-md-6"><h1>{data[0].name.common}</h1></span>
+                    <span className="col-md-6"><img className="Flag" src={'' + data[0].flags.png} /></span>
+                </div>
+                <div className="row">
+                    <span className="col-md-6">
+                        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                            <TileLayer
+                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={position}>
+                                <Popup>
+                                    A pretty CSS3 popup. <br /> Easily customizable.
+                                </Popup>
+                            </Marker>
+                        </MapContainer>
+                    </span>
+                </div>
+            </>
         )
     }
 
