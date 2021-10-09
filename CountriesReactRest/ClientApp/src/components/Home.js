cce2151c3133689c22adb8423ab1908a6dae2fb6
Joe <joe.andrew.bennett@gmail.com>
2021-10-09 12:03:58 -0500

    just added some more buttons so we can select what items we want to render

--- CountriesReactRest/ClientApp/src/components/Home.js
+++ CountriesReactRest/ClientApp/src/components/Home.js
@@ -9,6 +9,10 @@ export function Home() {
     const [error, setError] = useState(null);
     const [sortData, setSortData] = useState("name");
     const [orderBy, setOrderBy] = useState("asc");
+    const [showListView, setShowListView] = useState(false);
+    const [showChartView, setShowChartView] = useState(true);
+
+
 
     //for visited data fetch
     const [dataLocalAPI, setDataLocal] = useState(null);
@@ -50,27 +54,53 @@ export function Home() {
             <>
                 <div className="container-fluid">
                     <h1>Countries</h1>
+                    < br/>
                     <div className="row buttonRow">
-                        <div className="col-md-1">
-                            <button className="btn btn-lg btn-primary" onClick={() => setOrderBy( orderBy == "asc" ? "desc" : "asc" ) } >
-                                {orderBy}
-                            </button>
+                        <div className="col-md-1"></div>
+                        <div className="col-md-2">
+                            <button className={`buttonRow btn btn-sm ${showChartView? 'btn-primary' : 'btn-secondary'}`}
+                                onClick={() => setShowChartView(!showChartView)}>Chart</button>
                         </div>
                         <div className="col-md-2">
-                            <button className={`buttonRow btn btn-lg ${sortData === "name" ? 'btn-primary' : 'btn-secondary'}`}
-                                onClick={() => setSortData("name")}>Sort by Name</button>
-                        </div>
-                        <div className="col-md-2">
-                            <button className={`buttonRow btn btn-lg ${sortData === "population" ? 'btn-primary' : 'btn-secondary'}`}
-                                onClick={() => setSortData("population")}>Sort by Population</button>
-                        </div>
-                        <div className="col-md-2">
-                            <button className={`buttonRow btn btn-lg ${sortData === "size" ? 'btn-primary' : 'btn-secondary'}`}
-                                onClick={() => setSortData("size")}>Sort by Size</button>
+                            <button className={`buttonRow btn btn-sm ${showListView? 'btn-primary' : 'btn-secondary'}`}
+                                onClick={() => setShowListView(!showListView)}>List</button>
                         </div>
                     </div>
-                    <ChartView sortData={sortedData} sortedByProp={sortData} countryCount={10 }/>
-                    <CountryList sortData={sortedData} dataLocalAPI={dataLocalAPI} />
+                    <br />
+                    {(showChartView || showListView) &&
+                        <>
+                        <div className="row buttonRow">
+                            <div className="col-md-1">
+                                <button className="btn btn-sm btn-primary" onClick={() => setOrderBy(orderBy == "asc" ? "desc" : "asc")} >
+                                    {orderBy}
+                                </button>
+                            </div>
+                            <div className="col-md-2">
+                                <button className={`buttonRow btn btn-sm ${sortData === "name" ? 'btn-primary' : 'btn-secondary'}`}
+                                    onClick={() => setSortData("name")}>Sort by Name</button>
+                            </div>
+                            <div className="col-md-2">
+                                <button className={`buttonRow btn btn-sm ${sortData === "population" ? 'btn-primary' : 'btn-secondary'}`}
+                                    onClick={() => setSortData("population")}>Sort by Population</button>
+                            </div>
+                            <div className="col-md-2">
+                                <button className={`buttonRow btn btn-sm ${sortData === "size" ? 'btn-primary' : 'btn-secondary'}`}
+                                    onClick={() => setSortData("size")}>Sort by Size</button>
+                            </div>
+                        </div>
+                        <br />
+                        </>
+                    }
+                    {showChartView &&
+                        <div className="row">
+                            <ChartView sortData={sortedData} sortedByProp={sortData} countryCount={10} />
+                        </div>
+                    }
+                    {showListView &&
+                        <div className="row">
+                            <CountryList sortData={sortedData} dataLocalAPI={dataLocalAPI} />
+                        </div>
+                    }
                 </div>
             </>
         )
