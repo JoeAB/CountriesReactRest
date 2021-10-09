e687e5b8a94c6e15c76b01e50a0bb45502d6f22b
Joe <joe.andrew.bennett@gmail.com>
2021-10-09 07:11:46 -0500

    cleaning up index and adding recharts to project for part of phase 2 of my work on this

--- CountriesReactRest/ClientApp/src/index.js
+++ CountriesReactRest/ClientApp/src/index.js
@@ -8,31 +8,6 @@ const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 
-//https://restcountries.com/v3.1/all
-
-function Test1() {
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-        fetch('https://restcountries.com/v3.1/all').then((response) => response.json()).then(setData);
-    }, []);
-
-    if (data) {
-        return (
-            <ul>
-                {data.map((item) =>
-                (<li>
-                    {item.name.common}
-                </li>)
-                )}
-            </ul>
-        )
-    }
-    return <h1>Test nothing</h1>
-}
-
-
-
 ReactDOM.render(
     <BrowserRouter basename={baseUrl}>
         <App />
